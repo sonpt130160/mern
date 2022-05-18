88c8f033dcdd4c84cf23773996d1ab343bb7bee5
son <sonpthe130160@fpt.edu.vn>
2022-05-18 16:11:38 +0700

    update CRUD

--- client/src/todo/TaskList.jsx
+++ client/src/todo/TaskList.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import AddTask from "./AddTask";
-import { deleteTodoService, getAllTodosService } from "../services/todoService";
+import {
+  deleteTodoService,
+  getAllTodosService,
+  updateTodoService,
+} from "../services/todoService";
 
 export default class TaskList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       ListTask: null,
+      editTask: {},
     };
   }
 
@@ -19,49 +24,89 @@ export default class TaskList extends React.Component {
       this.setState({
         ListTask: res.data,
       });
-      console.log(res);
     });
   };
 
-  handleDeleteTask = async (task) => {
-    deleteTodoService(task._id);
-    this.getAllData();
+  handleEditTask = (todo) => {
+    const { editTask } = this.state;
+    const isEmpty = Object.keys(editTask).length === 0;
+    if (!isEmpty && editTask._id === todo._id) {
+      updateTodoService(todo._id, { task: editTask.task }).then((res) => {
+        getAllTodosService().then((res) => {
+          this.setState({
+            ListTask: res.data,
+            editTask: {},
+          });
+        });
+      });
+      return;
+    }
+    this.setState({
+      editTask: todo,
+    });
+  };
+
+  handleOnChangeEditTask = (event) => {
+    const editTaskCopy = { ...this.state.editTask };
+    editTaskCopy.task = event.target.value;
+    this.setState({
+      editTask: editTaskCopy,
+    });
+  };
+
+  handleDeleteTask = (task) => {
+    deleteTodoService(task._id).then((res) => {
+      this.getAllData();
+    });
   };
 
   render() {
-    const { ListTask } = this.state;
+    const { ListTask, editTask } = this.state;
+    const isEmpty = Object.keys(editTask).length === 0;
     return (
       <>
         <AddTask ListTask={this.state.ListTask} getAllData={this.getAllData} />
         <div>
           <h2>Task List</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Name of task</th>
-              </tr>
-            </thead>
-            <tbody>
-              {ListTask &&
-                ListTask.map((item, index) => {
-                  return (
-                    <tr key={item._id}>
-                      <td>
-                        {item.task}{" "}
-                        <button
-                          type="submit"
-                          onClick={() => {
-                            this.handleDeleteTask(item);
-                          }}
-                        >
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
+          {ListTask &&
+            ListTask.map((item, index) => {
+              return (
+                <div key={item._id}>
+                  {isEmpty === true ? (
+                    <span>{item.task}</span>
+                  ) : (
+                    <>
+                      {editTask._id === item._id ? (
+                        <span>
+                          <input
+                            onChange={(event) =>
+                              this.handleOnChangeEditTask(event)
+                            }
+                            value={editTask.task}
+                          />
+                        </span>
+                      ) : (
+                        <span>{item.task}</span>
+                      )}
+                    </>
+                  )}
+                  <button onClick={() => this.handleEditTask(item)}>
+                    {isEmpty === false && editTask._id === item._id
+                      ? "save"
+                      : "edit"}
+                  </button>
+
+                  <button
+                    type="submit"
+                    onClick={() => {
+                      this.handleDeleteTask(item);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              );
+            })}
         </div>
       </>
     );
